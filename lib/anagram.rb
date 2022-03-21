a9225f7c10f47addd57fe94a9a5e011e047d07cd
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-21 13:52:31 -0700

    commit tree f0085722c2fe56da139350c36eaf29eb73eb6334

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end 
+    
+    def match(array)
+        array.select do |string|
+            string.chars.sort == @word.chars.sort
+        end
+    end    
+end
+
